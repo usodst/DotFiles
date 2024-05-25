8cc806d6ac04fc7c4d9d349a7ace6425a8e0250f
Alex Egana <alexe@iarx.com>
2024-05-25 01:16:29 -0700

    Removes ui elements from file selection

--- nvim/lua/plugins/dap/coreclr/init.lua
+++ nvim/lua/plugins/dap/coreclr/init.lua
@@ -15,26 +15,13 @@ M.config = {
 		name = "launch - netcoredbg",
 		request = "launch",
 		program = function()
-			local conf = require("telescope.config").values
-			local GetDllFiles = function()
-				local dllFiles = vim.fs.find(function(name, path)
-					return name:match(".*dll$") and path:match("bin")
-				end, { type = "file", limit = math.huge })
-				--
-				return require("telescope.pickers")
-					.new({}, {
-						prompt_title = "Select dll",
-						finder = require("telescope.finders").new_table({
-							results = dllFiles,
-						}),
-						sorter = conf.generic_sorter({}),
-					})
-					:find()
-			end
-
-			-- return vim.fn.input("Path to dll: ", vim.fn.getcwd() .. "/bin/Debug/", "file")
-			local program = GetDllFiles()
-			return program
+			local dap = require("dap")
+			local path = vim.fn.input({
+				prompt = "Path to executable: ",
+				default = vim.fn.getcwd() .. "/",
+				completion = "file",
+			})
+			return (path and path ~= "") and path or dap.ABORT
 		end,
 	},
 }
