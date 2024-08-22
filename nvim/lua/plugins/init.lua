ab54dcd499ca8795464373b0ac5b216dfaffab94
Alex Egana <48147071+usodst@users.noreply.github.com>
2024-08-22 16:54:31 -0700

    Disables whichkey

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -65,28 +65,28 @@ require("lazy").setup({
 			},
 		},
 	},
-	{
-		"folke/which-key.nvim",
-		event = "VimEnter",
-		config = function()
-			require("which-key").setup()
-
-			-- Document existing key chains
-			require("which-key").add({
-				{ "<leader>c", group = "[C]ode" },
-				{ "<leader>d", group = "[D]ocument" },
-				{ "<leader>h", group = "Git [H]unk" },
-				{ "<leader>r", group = "[R]ename" },
-				{ "<leader>s", group = "[S]earch" },
-				{ "<leader>t", group = "[T]oggle" },
-				{ "<leader>w", group = "[W]orkspace" },
-			})
-			-- visual mode
-			require("which-key").add({
-				{ "<leader>h", desc = "Git [H]unk", mode = "v" },
-			})
-		end,
-	},
+	-- {
+	-- 	"folke/which-key.nvim",
+	-- 	event = "VimEnter",
+	-- 	config = function()
+	-- 		require("which-key").setup()
+	--
+	-- 		-- Document existing key chains
+	-- 		require("which-key").add({
+	-- 			{ "<leader>c", group = "[C]ode" },
+	-- 			{ "<leader>d", group = "[D]ocument" },
+	-- 			{ "<leader>h", group = "Git [H]unk" },
+	-- 			{ "<leader>r", group = "[R]ename" },
+	-- 			{ "<leader>s", group = "[S]earch" },
+	-- 			{ "<leader>t", group = "[T]oggle" },
+	-- 			{ "<leader>w", group = "[W]orkspace" },
+	-- 		})
+	-- 		-- visual mode
+	-- 		require("which-key").add({
+	-- 			{ "<leader>h", desc = "Git [H]unk", mode = "v" },
+	-- 		})
+	-- 	end,
+	-- },
 	{
 		"nvim-telescope/telescope.nvim",
 		event = "VimEnter",
