db0b0adc07a40b1f0e2e4019463ff28d11568c71
Alex Egana <48147071+usodst@users.noreply.github.com>
2024-08-27 17:59:26 -0700

    Displays hidden files in oil.nvim

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -608,7 +608,11 @@ require("lazy").setup({
 	{
 		"stevearc/oil.nvim",
 		config = function()
-			require("oil").setup()
+			require("oil").setup({
+				view_options = {
+					show_hidden = true,
+				},
+			})
 			vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 		end,
 		dependencies = { "nvim-tree/nvim-web-devicons" },
