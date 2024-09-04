66ca6e92c979ac4649e22c549e84b6c242399c13
Alex Egana <48147071+usodst@users.noreply.github.com>
2024-09-03 23:16:54 -0700

    Adds file auto complete and formatting

--- nvim/lua/plugins/dap/gdb/init.lua
+++ nvim/lua/plugins/dap/gdb/init.lua
@@ -1,21 +1,27 @@
 local M = {}
 
 M.adapter = {
-  type = "executable",
-  command = "gdb",
-  args = { "-i", "dap" }
+	type = "executable",
+	command = "gdb",
+	args = { "-i", "dap" },
 }
 
 M.config = {
-  {
-    name = "Launch",
-    type = "gdb",
-    request = "launch",
-    program = function()
-      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-    end,
-    cwd = "${workspaceFolder}",
-  }
+	{
+		name = "Launch",
+		type = "gdb",
+		request = "launch",
+		program = function()
+			local dap = require("dap")
+			local path = vim.fn.input({
+				prompt = "Path to executable: ",
+				default = vim.fn.getcwd() .. "/",
+				completion = "file",
+			})
+			return (path and path ~= "") and path or dap.ABORT
+		end,
+		cwd = "${workspaceFolder}",
+	},
 }
 
 return M
