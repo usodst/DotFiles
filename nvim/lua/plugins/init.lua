06592ac5bed661c00dc15ae3f004d3fc592db4b1
Alex Egana <48147071+usodst@users.noreply.github.com>
2024-10-15 15:05:16 -0700

    Adds key mappings for gitsigns

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -71,6 +71,22 @@ require("lazy").setup({
 				topdelete = { text = "‾" },
 				changedelete = { text = "~" },
 			},
+			on_attach = function(bufnr)
+				local gitsigns = require("gitsigns")
+				vim.keymap.set("n", "<leader>gs", function()
+					gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+				end, { desc = "Git [S]tage hunk", buffer = bufnr })
+				vim.keymap.set("n", "<leader>gr", function()
+					gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+				end, { desc = "Git [R]eset hunk", buffer = bufnr })
+				vim.keymap.set(
+					"n",
+					"<leader>ga",
+					gitsigns.stage_buffer,
+					{ desc = "Git St[a]ge Buffer", buffer = bufnr }
+				)
+				vim.keymap.set("n", "<leader>gd", gitsigns.reset_buffer, { desc = "Git Reset Buffer", buffer = bufnr })
+			end,
 		},
 	},
 	{
