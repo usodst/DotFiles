4071534cd665f2d1748c25ade5e3bbe9004c935e
Alex Egana <48147071+usodst@users.noreply.github.com>
2025-05-29 13:15:53 -0400

    Fixes Mason 2.0 breaking changes

--- nvim/lua/plugins/nvim-lspconfig/init.lua
+++ nvim/lua/plugins/nvim-lspconfig/init.lua
@@ -67,8 +67,6 @@ if vim.g.load_lsp then
 				end,
 			})
 
-			local capabilities = vim.lsp.protocol.make_client_capabilities()
-			capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 			local servers = {
 				bashls = {},
 				clangd = {},
@@ -151,20 +149,18 @@ if vim.g.load_lsp then
 				"stylua",
 				"yamlfmt",
 			})
-			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
 			require("mason-lspconfig").setup({
-				handlers = {
-					function(server_name)
-						local server = servers[server_name] or {}
-						-- This handles overriding only values explicitly passed
-						-- by the server configuration above. Useful when disabling
-						-- certain features of an LSP (for example, turning off formatting for tsserver)
-						server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
-						require("lspconfig")[server_name].setup(server)
-					end,
-				},
+				automatic_enable = vim.tbl_keys(servers or {}),
 			})
+
+			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
+
+			-- Installed LSPs are configured and enabled automatically with mason-lspconfig
+			-- The loop below is for overriding the default configuration of LSPs with the ones in the servers table
+			for server_name, config in pairs(servers) do
+				vim.lsp.config(server_name, config)
+			end
 		end,
 	}
 else
