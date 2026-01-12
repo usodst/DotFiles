146dba70a56bb6ce29d00e7517227b9e1c5871f0
Alex Egana <48147071+usodst@users.noreply.github.com>
2026-01-11 23:51:48 -0500

    Comments out unused key bindings

--- nvim/lua/plugins/gitsigns-nvim/init.lua
+++ nvim/lua/plugins/gitsigns-nvim/init.lua
@@ -5,16 +5,16 @@ return {
 		current_line_blame_opts = {
 			delay = 500,
 		},
-		on_attach = function(bufnr)
-			local gitsigns = require("gitsigns")
-			vim.keymap.set("n", "<leader>gs", function()
-				gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
-			end, { desc = "Git [S]tage hunk", buffer = bufnr })
-			vim.keymap.set("n", "<leader>gr", function()
-				gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
-			end, { desc = "Git [R]eset hunk", buffer = bufnr })
-			vim.keymap.set("n", "<leader>ga", gitsigns.stage_buffer, { desc = "Git St[a]ge Buffer", buffer = bufnr })
-			vim.keymap.set("n", "<leader>gd", gitsigns.reset_buffer, { desc = "Git Reset Buffer", buffer = bufnr })
-		end,
+		-- on_attach = function(bufnr)
+		-- 	local gitsigns = require("gitsigns")
+		-- 	vim.keymap.set("n", "<leader>gs", function()
+		-- 		gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+		-- 	end, { desc = "Git [S]tage hunk", buffer = bufnr })
+		-- 	vim.keymap.set("n", "<leader>gr", function()
+		-- 		gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+		-- 	end, { desc = "Git [R]eset hunk", buffer = bufnr })
+		-- 	vim.keymap.set("n", "<leader>ga", gitsigns.stage_buffer, { desc = "Git St[a]ge Buffer", buffer = bufnr })
+		-- 	vim.keymap.set("n", "<leader>gd", gitsigns.reset_buffer, { desc = "Git Reset Buffer", buffer = bufnr })
+		-- end,
 	},
 }
