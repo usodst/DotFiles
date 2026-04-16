afa47dee6e7df073d5251eb49591799be5332cfe
Alex Egana <48147071+usodst@users.noreply.github.com>
2026-04-15 23:17:25 -0400

    Adds dap breakpoint restoration

--- nvim/lua/plugins/auto-session/init.lua
+++ nvim/lua/plugins/auto-session/init.lua
@@ -1,9 +1,59 @@
 return {
-  "rmagatti/auto-session",
-  lazy = false,
+	"rmagatti/auto-session",
+	lazy = false,
 
-  ---enables autocomplete for opts
-  ---@module "auto-session"
-  ---@type AutoSession.Config
-  opts = {},
+	---enables autocomplete for opts
+	---@module "auto-session"
+	---@type AutoSession.Config
+	opts = {
+		save_extra_data = function(_)
+			local ok, breakpoints = pcall(require, "dap.breakpoints")
+			if not ok or not breakpoints then
+				return
+			end
+
+			local bps = {}
+			local breakpoints_by_buf = breakpoints.get()
+			for buf, buf_bps in pairs(breakpoints_by_buf) do
+				bps[vim.api.nvim_buf_get_name(buf)] = buf_bps
+			end
+			if vim.tbl_isempty(bps) then
+				return
+			end
+			local extra_data = {
+				breakpoints = bps,
+			}
+			return vim.fn.json_encode(extra_data)
+		end,
+
+		restore_extra_data = function(_, extra_data)
+			local json = vim.fn.json_decode(extra_data)
+
+			if json.breakpoints then
+				local ok, breakpoints = pcall(require, "dap.breakpoints")
+
+				if not ok or not breakpoints then
+					return
+				end
+				vim.notify("restoring breakpoints")
+				for buf_name, buf_bps in pairs(json.breakpoints) do
+					for _, bp in pairs(buf_bps) do
+						local line = bp.line
+						local opts = {
+							condition = bp.condition,
+							log_message = bp.logMessage,
+							hit_condition = bp.hitCondition,
+						}
+
+						local bufnr = vim.fn.bufnr(buf_name, true)
+						if vim.fn.bufloaded(bufnr) == 0 then
+							vim.api.nvim_buf_call(bufnr, vim.cmd.edit)
+						end
+
+						breakpoints.set(opts, bufnr, line)
+					end
+				end
+			end
+		end,
+	},
 }
