0582caaac5c1656c73520acf7a5d9a0b28aa7442
Alex Egana <48147071+usodst@users.noreply.github.com>
2026-04-16 23:00:22 -0400

    Changes nvim-treesitter to main

--- nvim/ftplugin/cpp.lua
+++ nvim/ftplugin/cpp.lua
@@ -2,7 +2,3 @@
 vim.treesitter.start()
 vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
 vim.wo[0][0].foldmethod = 'expr'
-
-vim.opt_local.tabstop = 4
-vim.opt_local.shiftwidth = 4
-vim.opt_local.expandtab = true
--- nvim/ftplugin/terraform.lua
+++ nvim/ftplugin/terraform.lua
@@ -1,3 +1,8 @@
+-- Tree-sitter
+vim.treesitter.start()
+vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+vim.wo[0][0].foldmethod = 'expr'
+
 vim.opt_local.tabstop = 2
 vim.opt_local.shiftwidth = 2
 vim.opt_local.expandtab = true
