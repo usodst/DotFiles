f792e395137471863f9ef79d6a33127854860686
Alex Egana <48147071+usodst@users.noreply.github.com>
2026-07-12 23:39:00 -0400

    Enables DPMS on mouse move and keyboard input

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -202,6 +202,8 @@ hl.config({
 
 hl.config({
 	misc = {
+		key_press_enables_dpms = true,
+		mouse_move_enables_dpms = true,
 		force_default_wallpaper = 0, -- Set to 0 or 1 to disable the anime mascot wallpapers
 		disable_hyprland_logo = true, -- If true disables the random hyprland logo / anime girl background. :(
 		disable_splash_rendering = true, -- Disable splash text
